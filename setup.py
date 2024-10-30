7172f93c47bc5067802f30871d109db45cf3e82c
Ankit Pasi <ankitpasi1@gmail.com>
2024-10-31 03:50:01 +0530

    Updates setup to include age binaries

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="1.0.0",
     packages=["agecrypt"],
     package_data={
-        "agecrypt": ["assets/*"],
+        "agecrypt": ["assets/*", "bin/*"],
     },
     install_requires=[
         "flet>=0.21.0",
